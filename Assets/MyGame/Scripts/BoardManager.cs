c3383d355239d163437d0515228b6ca38c3e4875
DESKTOP-RQJOPUJ\anton <antonkuzan@gmail.com>
2025-08-30 00:46:04 +0300

    Fixed the operation of spawning cells in the table. Now an array with cells is created, which can then be worked with

--- Assets/MyGame/Scripts/BoardManager.cs
+++ Assets/MyGame/Scripts/BoardManager.cs
@@ -2,19 +2,43 @@ using UnityEngine;
 
 public class BoardManager : MonoBehaviour
 {
-    [SerializeField] public int width = 3;
-    [SerializeField] public int height = 3;
-    [Space]
-    [SerializeField] private GameObject cellPrefab;
+    [SerializeField] public GameObject cell;
+    private GameObject[,] _cellPrefabs =  new GameObject[3,3];
+    private int width = 3;
+    private int height = 3;
 
     private void Start()
     {
+        for (int i = 0; i < _cellPrefabs.GetLength(0); i++)
+        {
+            for (int j = 0; j < _cellPrefabs.GetLength(1); j++)
+            {
+                _cellPrefabs[i,j] = cell;
+            }
+        }
+        
         for (int i = 0; i < width; i++)
         {
             for (int j = 0; j < height; j++)
             {
-                Instantiate(cellPrefab, transform);
+                GameObject prefabToSpawn = _cellPrefabs[i,j];
+                Instantiate(prefabToSpawn, transform);
             }
         }
     }  
+    
+    
+    
+    // private void Start()
+    // {
+    //     for (int i = 0; i < width; i++)
+    //     {
+    //         for (int j = 0; j < height; j++)
+    //         {
+    //             int random = Random.Range(0, crystalPrefabs.Length);
+    //             GameObject prefabToSpawn = crystalPrefabs[random];
+    //             Instantiate(prefabToSpawn, new Vector3(i, j, 0), Quaternion.identity, transform);
+    //         }
+    //     }
+    // }  
 }
